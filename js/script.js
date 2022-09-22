c855a5fa28eff0c53c8f9084a4767be3f36eeab7
JULIΛN (Pinguin2001) <75797743+Pinguin2001@users.noreply.github.com>
2022-09-22 20:23:59 +0200

    Upload website

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  /* If navigate= argument is present in the url, navigate to that page */
+  var pageName = getPageString("navigate");
+  if (pageName !== null)
+  {
+    openPage(pageName);
+  }
+  else {
+    openPage('Home');
+  }
+  /* Responive Nav */
+  opencloseNav();
+});
+
+$(window).resize(function() {
+  opencloseNav();
+});
+
+function opencloseNav() {
+  if ($(window).width() > 700) {
+    openNav();
+  }
+  else {
+    closeNav();
+  }
+}
+
+function openNav() {
+  $('.Nav').width("250px");
+}
+
+function closeNav() {
+  $('.Nav').width("0px");
+}
+
+function openPage(pageName) {
+  var page;
+  page = $(".page");
+  for (i = 0; i < page.length; i++) {
+    $(page[i]).hide();
+  }
+  if ($(window).width() < 700) {
+    closeNav();
+  }
+  $("#" + pageName).show();
+}
+
+function getPageString(String) {
+  return decodeURI((new RegExp("[?]" + String + "=" + "([^&]+?)($)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
